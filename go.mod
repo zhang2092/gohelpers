deb1529d7cec8e01d8618a67315542cdb1df888b
kenneth <lyylhl.02380324.zhang@outlook.com>
2022-05-10 15:22:12 +0800

    update

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/zhang2092/gohelpers
 
-go 1.17
+go 1.18
 
 require (
 	github.com/aead/chacha20poly1305 v0.0.0-20201124145622-1a5aba2a8b29
@@ -11,6 +11,7 @@ require (
 	github.com/lib/pq v1.10.4
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/o1egl/paseto v1.0.0
+	github.com/stretchr/testify v1.7.0
 	go.uber.org/zap v1.19.1
 	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8
@@ -21,6 +22,7 @@ require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
 	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
@@ -34,10 +36,12 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20210915083310-ed5796bab164 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- pkg/convertor/convertor.go
+++ pkg/convertor/convertor.go
@@ -38,15 +38,15 @@ func ToBytes(value any) ([]byte, error) {
 	case float32:
 		number := float32(v.Float())
 		bits := math.Float32bits(number)
-		bytes := make([]byte, 4)
-		binary.BigEndian.PutUint32(bytes, bits)
-		return bytes, nil
+		bys := make([]byte, 4)
+		binary.BigEndian.PutUint32(bys, bits)
+		return bys, nil
 	case float64:
 		number := v.Float()
 		bits := math.Float64bits(number)
-		bytes := make([]byte, 8)
-		binary.BigEndian.PutUint64(bytes, bits)
-		return bytes, nil
+		bys := make([]byte, 8)
+		binary.BigEndian.PutUint64(bys, bits)
+		return bys, nil
 	case bool:
 		return strconv.AppendBool([]byte{}, v.Bool()), nil
 	case string:
@@ -225,62 +225,4 @@ func ColorRGBToHex(red, green, blue int) string {
 	}
 
 	return "#" + r + g + b
-}
-
-// Strval 获取变量的字符串值
-// 浮点型 3.0将会转换成字符串3, "3"
-// 非数值或字符类型的变量将会被转换成JSON格式字符串
-func Strval(value interface{}) string {
-	// interface 转 string
-	var key string
-	if value == nil {
-		return key
-	}
-
-	switch value.(type) {
-	case float64:
-		ft := value.(float64)
-		key = strconv.FormatFloat(ft, 'f', -1, 64)
-	case float32:
-		ft := value.(float32)
-		key = strconv.FormatFloat(float64(ft), 'f', -1, 64)
-	case int:
-		it := value.(int)
-		key = strconv.Itoa(it)
-	case uint:
-		it := value.(uint)
-		key = strconv.Itoa(int(it))
-	case int8:
-		it := value.(int8)
-		key = strconv.Itoa(int(it))
-	case uint8:
-		it := value.(uint8)
-		key = strconv.Itoa(int(it))
-	case int16:
-		it := value.(int16)
-		key = strconv.Itoa(int(it))
-	case uint16:
-		it := value.(uint16)
-		key = strconv.Itoa(int(it))
-	case int32:
-		it := value.(int32)
-		key = strconv.Itoa(int(it))
-	case uint32:
-		it := value.(uint32)
-		key = strconv.Itoa(int(it))
-	case int64:
-		it := value.(int64)
-		key = strconv.FormatInt(it, 10)
-	case uint64:
-		it := value.(uint64)
-		key = strconv.FormatUint(it, 10)
-	case string:
-		key = value.(string)
-	case []byte:
-		key = string(value.([]byte))
-	default:
-		newValue, _ := json.Marshal(value)
-		key = string(newValue)
-	}
-	return key
 }
